6a28e2e7978131eb5cfe10e8a6d803a34207c09a
Pragyesh Kumar Seth <116973800+pragyesh7753@users.noreply.github.com>
2023-11-18 21:11:11 +0530

    new

--- mar20.js
+++ mar20.js
@@ -1,9 +1,24 @@
-try {
-    let x = undefined;
-    console.log(x[0]);
-} catch(err) {
-    console.log("Error handling in catch",err);
-} finally {
-    console.log("finally always gets executed");
-}
-console.log("end");
+// // Try, catch, finally
+// try {
+//     let x = undefined;
+//     console.log(x[0]);
+// } catch (err) {
+//     console.log("Error handling in catch", err);
+// } finally {
+//     console.log("finally always gets executed");
+// }
+// console.log("end");
+
+// // Use of Try Catch in Real World Application
+// try {
+//     console.log("hello");
+//     console.log(a);
+//     console.log("ending try");
+// } catch {
+//     console.log("handled");
+// }finally{
+//     console.log("finally");
+// }
+// console.log("end");
+
+// Defining a Class, Class Instantiation
