791d6474df639f3572b6f2b7da82a12c6dd1c13e
Pragyesh Kumar Seth <116973800+pragyesh7753@users.noreply.github.com>
2023-12-10 19:38:10 +0530

    new

--- mar20.js
+++ mar20.js
@@ -21,4 +21,27 @@
 // }
 // console.log("end");
 
-// Defining a Class, Class Instantiation
+// // Defining a Class, Class Instantiation
+class Product {
+    // name;
+    // price;
+    // rating
+
+    constructor(n, p, r) {
+        console.log("Calling a constructor");
+        this.name = n;
+        this.price = p;
+        this.rating = r;
+    }
+
+    // behaviours -> functions -> member functions
+    display() {
+        console.log("this refers to", this);
+        console.log("displaying the current product", this.name, this.price, this.rating);
+    }
+}
+const p = new Product("iphone", 100000, 5);  // new --> creates an empty plain object
+// -> in the above piece of code we are clling the constructor method
+console.log(p);
+p.display();
+// // Class Constructor, Deafault Value With Constructor
